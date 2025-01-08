e3c6c0507a8e35ce18266d37a30dc4760670ad9e
Ramees C <rameesc985@gmail.com>
2025-01-08 13:04:02 +0530

    recommentedcomponent design created

---
+++ src/components/Recommented/Recommented.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import "./Recommented.css"
+
+import thambnail1 from "../../assets/thumbnail1.png"
+import thambnail2 from "../../assets/thumbnail2.png"
+import thambnail3 from "../../assets/thumbnail3.png"
+import thambnail4 from "../../assets/thumbnail4.png"
+import thambnail5 from "../../assets/thumbnail5.png"
+import thambnail6 from "../../assets/thumbnail6.png"
+import thambnail7 from "../../assets/thumbnail7.png"
+import thambnail8 from "../../assets/thumbnail8.png"
+
+function Recommented() {
+  return (
+    <div className='recommented'>
+        <div className="side-video-list">
+            <img src={thambnail1} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thambnail2} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thambnail3} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thambnail4} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thambnail5} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thambnail6} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thambnail7} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+
+        <div className="side-video-list">
+            <img src={thambnail8} alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web development</h4>
+                <p>GreatStack</p>
+                <p>199k Views</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Recommented
--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Video.css"
 import PlayVideo from '../../components/PlayVideo/PlayVideo'
+import Recommented from '../../components/Recommented/Recommented'
 
 function Video() {
   return (
     <div className='play-container'>
         <PlayVideo />
+        <Recommented />
     </div>
   )
 }
