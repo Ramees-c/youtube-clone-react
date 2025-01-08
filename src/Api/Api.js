89f45c1294831548c284d163eacf7264088bd99b
Ramees C <rameesc985@gmail.com>
2025-01-08 16:03:12 +0530

    youtube api implimented and feed dynamically created

---
+++ src/Api/Api.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'AIzaSyB6vYaa5ThmLTepXXAYjf2XEgVmW59ZJUg';
--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
 
 import thambnail1 from "../../assets/thumbnail1.png";
@@ -10,139 +10,42 @@ import thambnail6 from "../../assets/thumbnail6.png";
 import thambnail7 from "../../assets/thumbnail7.png";
 import thambnail8 from "../../assets/thumbnail8.png";
 import { Link } from "react-router-dom";
+import { API_KEY } from "../../Api/Api";
+import axios from "axios";
+import { value_converter } from "../../Data";
+import moment from "moment";
 
-function Feed() {
+function Feed({ category }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+        const response = await axios.get(videoList_url);
+        // console.log(response.data.items, "itemss");
+        setData(response.data.items);
+      } catch (error) {
+         console.error(error.message)
+      }
+    };
+    fetchData();
+  }, [category]);
+
   return (
     <div className="feed">
-      <Link to={`video/20/4521`} className="card">
-        <img src={thambnail1} alt="" />
+      {data.map((item, index) => {
+        return (
+          <Link to={`video/${item.snippet.categoryId}/${item.id}`} key={index} className="card">
+        <img src={item.snippet.thumbnails.medium.url} alt="" />
         <h2>
-          Best channel to learn coding that help you to be a web developer
+          {item.snippet.title}
         </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
+        <h3>{item.snippet.channelTitle}</h3>
+        <p>{value_converter(item.statistics.viewCount)} views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
       </Link>
-      <div className="card">
-        <img src={thambnail2} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail3} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail4} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail5} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail6} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail7} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail8} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-
-      <div className="card">
-        <img src={thambnail1} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail2} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail3} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail4} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail5} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail6} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail7} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thambnail8} alt="" />
-        <h2>
-          Best channel to learn coding that help you to be a web developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
+        )
+      })}
     </div>
   );
 }
